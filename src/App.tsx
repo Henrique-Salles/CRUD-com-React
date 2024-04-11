6722a001cb2d61b5f15c374ace6487dc359527b7
Henrique-Salles <henriquesalles1520@gmail.com>
2024-04-11 18:48:42 -0300

    edit items

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,46 @@ export default function App() {
     "Tirar o lixo",
     "Ajudar no almoço",
   ]);
+  const [editTask, setEditTask] = useState({
+    enabled: false,
+    task: "",
+  });
 
   function handleRegister() {
     if (!input) {
     }
 
+    if (editTask.enabled) {
+      handleSaveEdit();
+      return;
+    }
     setTasks((tarefas) => [...tarefas, input]);
     setInput("");
   }
   function handleDelet(item: string) {
     const removeTask = tasks.filter((task) => task !== item);
     setTasks(removeTask);
+  }
+  function handleEdit(item: string) {
+    setInput(item);
+    setEditTask({
+      enabled: true,
+      task: item,
+    });
+  }
+  function handleSaveEdit() {
+    const findIndexTask = tasks.findIndex((task) => task === editTask.task);
+    const allTasks = [...tasks];
+
+    allTasks[findIndexTask] = input;
+    setTasks(allTasks);
+
+    setEditTask({
+      enabled: false,
+      task: "",
+    });
+
+    setInput("");
   }
 
   return (
@@ -32,12 +61,15 @@ export default function App() {
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
-      <button onClick={handleRegister}>Adicionar tarefa</button>
+      <button onClick={handleRegister}>
+        {editTask.enabled ? "Atualizar Tarefa" : "Adicionar Tarefa"}
+      </button>
       <hr />
 
       {tasks.map((item, index) => (
         <section key={item}>
           <span>{item}</span>
+          <button onClick={() => handleEdit(item)}>Editar</button>
           <button onClick={() => handleDelet(item)}>Excluir</button>
         </section>
       ))}
